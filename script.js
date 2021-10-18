b910b314191040075a5ee7d5359201f8271f46d8
Marsigli <marsigli1988@gmail.com>
2021-10-18 14:37:48 -0400

    functions set up to populate database objects

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const { listenerCount } = require("process")
+
 
 const url = "http://localhost:3000/movies"
 
@@ -9,11 +9,15 @@ let movies = document.createElement('li')
 const form = document.querySelector('#movie-form')
 
 function renderMovies(movie) {
+    //assigning an individual ID to each movie
     movies.id = movie.id
-    // style here
+    //calling the renderMovieText function
+    renderMovieText(movies, movie)
+    movieList.appendChild(movies) 
 
 }
 
+//this is called once the page is loaded and lists out on the page all of your movies in your db.json
 function listMovies() {
     fetch(url)
         .then(res => res.json())
@@ -28,9 +32,8 @@ function listMovies() {
 
 // unfinished function
 function renderMovieText(li, movie) {
-    li.innerHTML = `    <span>${movie.body}</span>${movie.updated_at ? moment(movie.updated_at).format('MMM DD,YYYY') : ""
-}<i class="ml2 dark-red fas fa-times delete" ></i > <i class="ml3 fas fa-edit edit"></i>
-
-    
-    `
+    li.innerHTML = `<span>${movie.title}</span>${movie.updated_at ? moment(movie.updated_at).format('MMM DD,YYYY') : ""
+}<i class="ml2 dark-red fas fa-times delete" ></i > <i class="ml3 fas fa-edit edit"></i>`
 }
+
+listMovies()
